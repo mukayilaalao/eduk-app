e13da4a6e5b24337f3a2974f376760822e797960
Mukayila ALAO <mukayilalao@yahoo.fr>
2022-07-08 10:27:38 -0400

    show relative messages when trying to submit empty forms

--- frontend/src/components/CreateResource.js
+++ frontend/src/components/CreateResource.js
@@ -4,182 +4,216 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import Button from "@mui/material/Button";
 import "../css/createResource.css";
+import GeneralShowMessage from "./GeneralShowMessage";
 
 const API = process.env.REACT_APP_API_URL;
 function CreateResource() {
-	// check box
-	const [highSchool, setHighSchool] = useState(false);
-	const [college, setCollege] = useState(false);
-	const [continuedLearning, setContinuedLearning] = useState(false);
-	const [resourceCategory, setResourceCategory] = useState("Program");
+  // check box
+  const [highSchool, setHighSchool] = useState(false);
+  const [college, setCollege] = useState(false);
+  const [continuedLearning, setContinuedLearning] = useState(false);
+  const [resourceCategory, setResourceCategory] = useState("Program");
+  const [open, setOpen] = useState(false);
 
-	// select drop down
-	const options = ["Program", "Class", "Scholarship"];
-	// option state
+  // select drop down
+  const options = ["Program", "Class", "Scholarship"];
+  // option state
 
-	// all state to send
-	const [resource, setResource] = useState({
-		resource_name: "",
-		start_datetime: "",
-		end_datetime: "",
-		url: "",
-		description: "",
-		resource_category: "",
-		resourcefor: [],
-		is_verified: false,
-	});
+  // all state to send
+  const [resource, setResource] = useState({
+    resource_name: "",
+    start_datetime: "",
+    end_datetime: "",
+    url: "",
+    description: "",
+    resource_category: "",
+    resourcefor: [],
+    is_verified: false,
+  });
 
-	resource.resource_category = resourceCategory;
+  resource.resource_category = resourceCategory;
 
-	if (highSchool) {
-		resource.resourcefor[0] = "highschool";
-	} else {
-		resource.resourcefor[0] = "null";
-	}
-	if (college) {
-		resource.resourcefor[1] = "college";
-	} else {
-		resource.resourcefor[1] = "null";
-	}
-	if (continuedLearning) {
-		resource.resourcefor[2] = "continued learning";
-	} else {
-		resource.resourcefor[2] = "null";
-	}
+  if (highSchool) {
+    resource.resourcefor[0] = "highschool";
+  } else {
+    resource.resourcefor[0] = "null";
+  }
+  if (college) {
+    resource.resourcefor[1] = "college";
+  } else {
+    resource.resourcefor[1] = "null";
+  }
+  if (continuedLearning) {
+    resource.resourcefor[2] = "continued learning";
+  } else {
+    resource.resourcefor[2] = "null";
+  }
 
-	const navigate = useNavigate();
-	const handleTextChange = (e) => {
-		setResource({ ...resource, [e.target.id]: e.target.value });
-	};
+  const navigate = useNavigate();
+  const handleTextChange = (e) => {
+    setResource({ ...resource, [e.target.id]: e.target.value });
+  };
 
-	const selectChange = (e) => {
-		setResourceCategory(e.target.value);
-	};
+  const selectChange = (e) => {
+    setResourceCategory(e.target.value);
+  };
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		//add a new resource to the database
-		axios
-			.post(`${API}/resources`, resource)
-			.then((res) => navigate("/resources"))
-			.catch((e) => console.log(e));
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //make sure the form is not empty
+    console.log(resource);
+    const {
+      resource_name,
 
-	return (
-		<div className="newForm">
-			<h1>Thank you for Adding Resource!</h1>
-			<form onSubmit={handleSubmit} className="create-resource-form">
-				<div>
-					<input
-						id="resource_name"
-						placeholder="Resource Name"
-						value={resource.resource_name}
-						onChange={handleTextChange}
-						type="text"
-					/>
-				</div>
-				<div>
-					<input
-						id="start_datetime"
-						placeholder="Start Date and Time"
-						value={resource.start_datetime}
-						onChange={handleTextChange}
-						type="text"
-					/>
-				</div>
+      url,
+      start_datetime,
+      end_datetime,
+    } = resource;
+    if (!resource_name || !start_datetime || !end_datetime || !url) {
+      setOpen(true);
+      return;
+    }
 
-				<div>
-					<input
-						id="end_datetime"
-						placeholder="End Date and Time"
-						value={resource.end_datetime}
-						onChange={handleTextChange}
-						type="text"
-					/>
-				</div>
+    //add a new resource to the database
+    axios
+      .post(`${API}/resources`, resource)
+      .then((res) => navigate("/resources"))
+      .catch((e) => console.log(e));
+  };
+  //close the message box
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
 
-				<div>
-					<input
-						id="url"
-						placeholder="Url"
-						value={resource.url}
-						onChange={handleTextChange}
-						type="text"
-					/>
-				</div>
+    setOpen(false);
+  };
 
-				<div>
-					<textarea
-						id="description"
-						placeholder="Resource Description"
-						value={resource.description}
-						onChange={handleTextChange}
-						type="text"
-					/>
-				</div>
+  return (
+    <div className="newForm">
+      <GeneralShowMessage
+        severity="warning"
+        message={"Please fill out the form correctly!"}
+        open={open}
+        handleClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      />
+      <h1>Thank you for Adding Resource!</h1>
+      <form onSubmit={handleSubmit} className="create-resource-form">
+        <div>
+          <input
+            id="resource_name"
+            placeholder="Resource Name"
+            value={resource.resource_name}
+            onChange={handleTextChange}
+            type="text"
+          />
+        </div>
+        <div>
+          <input
+            id="start_datetime"
+            placeholder="Start Date and Time"
+            value={resource.start_datetime}
+            onChange={handleTextChange}
+            type="text"
+          />
+        </div>
 
-				{/* options  */}
+        <div>
+          <input
+            id="end_datetime"
+            placeholder="End Date and Time"
+            value={resource.end_datetime}
+            onChange={handleTextChange}
+            type="text"
+          />
+        </div>
 
-				<div className="select-container">
-					<select onChange={selectChange}>
-						{options.map((option) => (
-							<option value={option}>{option}</option>
-						))}
-					</select>
-				</div>
+        <div>
+          <input
+            id="url"
+            placeholder="Url"
+            value={resource.url}
+            onChange={handleTextChange}
+            type="text"
+          />
+        </div>
 
-				{/* check box  */}
+        <div>
+          <textarea
+            id="description"
+            placeholder="Resource Description"
+            value={resource.description}
+            onChange={handleTextChange}
+            type="text"
+          />
+        </div>
 
-				<div className="create-resource-checkboxes">
-					<div>
-						<label className="labelResource" htmlFor="highschool">
-							High School
-						</label>
-						<input
-							id="highschool"
-							value="highschool"
-							type="checkbox"
-							onChange={(e) => setHighSchool(e.target.checked)}
-						/>
-					</div>
+        {/* options  */}
 
-					<div>
-						<label className="labelResource" htmlFor="college">
-							College
-						</label>
-						<input
-							id="college"
-							type="checkbox"
-							name="college"
-							onChange={(e) => setCollege(e.target.checked)}
-						/>
-					</div>
+        <div className="select-container">
+          <select onChange={selectChange}>
+            {options.map((option) => (
+              <option value={option}>{option}</option>
+            ))}
+          </select>
+        </div>
 
-					<div>
-						<label className="labelResource" htmlFor="continuedLearning">
-							Continued Learning
-						</label>
-						<input
-							id="continuedLearning"
-							type="checkbox"
-							name="continuedLearning"
-							onChange={(e) => setContinuedLearning(e.target.checked)}
-						/>
-					</div>
-				</div>
+        {/* check box  */}
 
-				{/* check box  */}
+        <div className="create-resource-checkboxes">
+          <div>
+            <label className="labelResource" htmlFor="highschool">
+              High School
+            </label>
+            <input
+              id="highschool"
+              value="highschool"
+              type="checkbox"
+              onChange={(e) => setHighSchool(e.target.checked)}
+            />
+          </div>
 
-				<Button
-					type="submit"
-					variant="contained"
-					style={{ width: "100px", color: "black", backgroundColor: "white" }}
-				>
-					Submit
-				</Button>
-			</form>
-		</div>
-	);
+          <div>
+            <label className="labelResource" htmlFor="college">
+              College
+            </label>
+            <input
+              id="college"
+              type="checkbox"
+              name="college"
+              onChange={(e) => setCollege(e.target.checked)}
+            />
+          </div>
+
+          <div>
+            <label className="labelResource" htmlFor="continuedLearning">
+              Continued Learning
+            </label>
+            <input
+              id="continuedLearning"
+              type="checkbox"
+              name="continuedLearning"
+              onChange={(e) => setContinuedLearning(e.target.checked)}
+            />
+          </div>
+        </div>
+
+        {/* check box  */}
+
+        <Button
+          type="submit"
+          variant="contained"
+          style={{ width: "100px", color: "black", backgroundColor: "white" }}
+        >
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateResource;
